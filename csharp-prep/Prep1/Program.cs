fdacdbfef588b46f06e421d83284bc201c29642b
Rex Oldham <old19001@byui.edu>
2026-01-15 02:50:55 -0700

    prep 1 compleated

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,17 @@
-using System;
-
+using System; 
+//made by w00f
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+    // prompt user for name
+        Console.Write("What is your first name?");
+        string First = Console.ReadLine();
+        Console.Write("What is your Last name?");
+        string Last = Console.ReadLine();
+
+    // display output
+        //Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine($"Your name is {Last}, {First} {Last}.");
     }
 }
