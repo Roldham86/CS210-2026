2a55b8d7396a27776c7ed7a68ccd72bf1f98e04a
Rex Oldham <old19001@byui.edu>
2026-01-29 01:32:11 -0700

    small gram corection made

--- prepare/Learning02/resume.cs
+++ prepare/Learning02/resume.cs
@@ -12,7 +12,7 @@ public class Resume
     {
         Console.WriteLine($"Name: {_name}");
         Console.WriteLine("Jobs:");
-
+    // loop through jobs    
         foreach (Job job in _jobs)
         {
             job.Display();
