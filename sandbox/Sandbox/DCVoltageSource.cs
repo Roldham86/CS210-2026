6bddd23522d780f06c6b329adb77e099f4d668e2
Wolf <Wolf@bush.works>
2026-03-29 00:04:10 -0600

    fixd? dc voltage? maby??

--- sandbox/Sandbox/DCVoltageSource.cs
+++ sandbox/Sandbox/DCVoltageSource.cs
@@ -1,4 +1,3 @@
-// DCVoltageSource.cs
 using System;
 
 public class DCVoltageSource : CircuitComponent
@@ -14,6 +13,21 @@ public class DCVoltageSource : CircuitComponent
     public double GetVoltage()
     {
         return _voltage;
+    }
+
+    public override bool CanCombineWith(CircuitComponent other)
+    {
+        return false;
+    }
+
+    public override CircuitComponent CombineSeries(CircuitComponent other, int newComponentId, string outerNet1, string outerNet2)
+    {
+        throw new InvalidOperationException("DC voltage sources are not supported in series reduction.");
+    }
+
+    public override CircuitComponent CombineParallel(CircuitComponent other, int newComponentId)
+    {
+        throw new InvalidOperationException("DC voltage sources are not supported in parallel reduction.");
     }
 
     public override string GetComponentType()
